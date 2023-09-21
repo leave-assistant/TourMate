6d7d3ff6c472cd226ee5a6a54c4a479a91091739
Your Name <you@example.com>
2023-09-21 16:10:07 +0900

    마이페이지 UI

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import people from 'src/pages/MyPages/people.png';
 
 const MyPage = () => {
   return (
@@ -8,18 +7,18 @@ const MyPage = () => {
       <MenuBar></MenuBar>
       <MyPageContent>
         <Title>MY 투어메이트</Title>
-        <Image src={people} />
+        <Image><img src="/MyPage_Image/people.png"/></Image>
         <Profile>홍길동<br/>20대/남성</Profile>
         <Introduce>
-          <Q>여행스타일</Q> <A>여행스타일을 입력해주세요</A>
-          <Q>MBTI</Q> <A>MBTI을 입력해주세요</A>
-          <Q>소개글</Q> <A>소개글을 입력해주세요</A>
+          <Question>여행스타일</Question> <Answer>여행스타일을 입력해주세요</Answer>
+          <Question>MBTI</Question> <Answer>MBTI을 입력해주세요</Answer>
+          <Question>소개글</Question> <Answer>소개글을 입력해주세요</Answer>
         </Introduce>
-        <Record>
-          <T>이용 기록</T>
+        <History>
+          <UseHistory>이용 기록</UseHistory>
           <Button>이전 코스 내역</Button>
           <Button>내가 작성한 리뷰</Button>
-        </Record>
+        </History>
       </MyPageContent>
     </MyPageWrapper>
   );
@@ -56,17 +55,19 @@ const Title = styled.div`
   border-bottom: 1px solid #0160D6
 `;
 
-const Image = styled.img`
-  width: 131px;
-  height: 131px;
+const Image = styled.div`
+  width: 130px;
+  height: 130px;
   margin-top: 40px;
-  text-align: center;
+  display: flex;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
 const Profile = styled.div`
   width: 100%;
   height: 70px;
-  margin-top: 15px;
+  margin-top: 10px;
   font-size: 24px;
   font-weight: normal; 
   text-align: center;
@@ -79,7 +80,7 @@ const Introduce = styled.div`
   border-bottom: 1px solid #000000
 `;
 
-const Q = styled.div`
+const Question = styled.div`
   width: 100%;
   margin-top: 25px;
   margin-left: 30px;
@@ -88,7 +89,7 @@ const Q = styled.div`
   color: #000000
 `;
 
-const A = styled.div`
+const Answer = styled.div`
   width: 100%;
   margin: 5px;
   font-size: 22px;
@@ -97,12 +98,12 @@ const A = styled.div`
   color: #D3D3D3
 `;
 
-const Record = styled.div`
+const History = styled.div`
   width: 100%;
   margin-top: 35px;
 `;
 
-const T = styled.div`
+const UseHistory = styled.div`
   width: 100%;
   margin-left: 25px;
   font-size: 26px;
