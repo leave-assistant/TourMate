1a1d14a0b6b5ddb6d8ad6bd92089762b9d6486b6
jiwon0811 <shery0406@naver.com>
2023-10-10 14:07:51 +0900

    동행거절버튼추가

--- src/pages/Matching/ConnectionPeople.tsx
+++ src/pages/Matching/ConnectionPeople.tsx
@@ -1,51 +1,80 @@
-import React , { useState }from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+
+class Persona {
+  location: string;
+  name: string;
+  timeline: string;
+  constructor(location: string, name: string, timeline: string) {
+    this.location = location;
+    this.name = name;
+    this.timeline = timeline;
+  }
+}
+
+const personas = [
+  new Persona('강릉 여행지', '이지원', '08:00 기상  -10:00 학교 - 13:00 점심 - 14:00 오후 수업 17:00 -귀가'),
+  new Persona('서울 여행지', '이몽룡', '08:00 기상   -10:00 학교 - 13:00 점심 - 14:00 오후 수업 17:00 -귀가'),
+  new Persona('부산 여행지', '홍길동', '08:00 기상 -10:00 학교 - 13:00 점심 - 14:00 오후 수업 17:00 -귀가'),
+  new Persona('제주도 여행지', '황진이', '08:00 기상 -10:00 학교 - 13:00 점심 - 14:00 오후 수업 17:00 -귀가'),
+];
 
 const ConnectPeople = () => {
   const [message, setMessage] = useState('');
 
   const requestButtonClick = () => {
-    alert("클릭");
+    alert('동행 요청을 보냈습니다.');
     setMessage('동행요청');
   };
+
+  const rejectButtonClick = () => {
+    alert('동행 거절을 보냈습니다.');
+    setMessage('동행거절');
+  };
+
+  const maxDisplayedPersona = personas.slice(0, 3);
+
   return (
-    <Box>
-      <Content>
-        <ImageContainer>
-          <img
-            src="/User_Image/Profile_Blue.jpg"
-            alt="My Image"
-            style={{
-              width: '100px',
-              height: '100px',
-              borderRadius: '70%',
-            }}
-          />
-        </ImageContainer>
-        <UserInfo>
-          <Location>강릉 여행지</Location>
-          <Name>이지원</Name>
-          <Button onClick={requestButtonClick}>동행요청</Button>
-        </UserInfo>
-      </Content>
-      <TimeLine>08:00 기상 -10:00 학교 - 13:00 점심
-         - 14:00 오후 수업 17:00 -귀가 </TimeLine>
-    </Box>
+    <div>
+      {maxDisplayedPersona.map((individual, index) => (
+        <Box key={index}>
+          <Content>
+            <ImageContainer>
+              <img
+                src="/User_Image/Profile_Blue.jpg"
+                alt="My Image"
+                style={{
+                  width: '100px',
+                  height: '100px',
+                  borderRadius: '70%',
+                }}
+              />
+            </ImageContainer>
+            <UserInfo>
+              <Location>{individual.location}</Location>
+              <Name>{individual.name}</Name>
+              <Button onClick={requestButtonClick}>동행요청</Button>
+              <RejectButton onClick={rejectButtonClick}>동행거절</RejectButton>
+            </UserInfo>
+          </Content>
+          <TimeLine>{individual.timeline}</TimeLine>
+        </Box>
+      ))}
+    </div>
   );
 };
 
-export default ConnectPeople;
 const Content = styled.div`
   display: flex;
   align-items: center;
 `;
+
 const Box = styled.div`
   margin-left: 20px;
   position: relative;
   width: 390px;
   height: 200px;
   border: 1px solid black;
-  
 `;
 
 const ImageContainer = styled.div`
@@ -53,7 +82,7 @@ const ImageContainer = styled.div`
   margin-top: 10px;
 `;
 
-const UserInfo = styled.div` 
+const UserInfo = styled.div`
   margin-left: 15px;
   margin-top: 10px;
 `;
@@ -84,16 +113,35 @@ const TimeLine = styled.p`
 const Button = styled.button`
   width: 90px;
   height: 30px;
-  background-color: #0160D6;
+  background-color: #0160d6;
   color: white;
   border-radius: 10px;
   font-size: 16px;
   font-weight: bold;
   border: none;
-  position: absolute; //위치고정
+  position: absolute;
   top: 25px;
   right: 5px;
   &:hover {
     background-color: #003f9e;
   }
 `;
+
+const RejectButton = styled.button`
+  width: 90px;
+  height: 30px;
+  background-color: red;
+  color: white;
+  border-radius: 10px;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  position: absolute;
+  top: 65px;
+  right: 5px;
+  &:hover {
+    background-color: #990000;
+  }
+`;
+
+export default ConnectPeople;
