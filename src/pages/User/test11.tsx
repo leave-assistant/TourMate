48ccd38df8bfabefe1a4329408b45dfe15053571
Hyemin Yang <asitwas729@gmail.com>
2023-11-06 15:44:22 +0900

    유저확인 컴포넌트 분리, 사용자 정보 불러오기 완료

---
+++ src/pages/User/test11.tsx
@@ -0,0 +1,16 @@
+import { UserProvider, useUser } from './UserContext';
+
+function UserProfile() {
+	const user = useUser();
+
+	// user를 사용하여 사용자 데이터 렌더링
+	return (
+		<div>
+		<p>사용자 UID: {user?.nickname}</p>
+		<p>이메일: {user?.age}</p>
+		{/* 다른 사용자 데이터 필드 렌더링 */}
+		</div>
+	);
+}
+
+export default UserProfile;
